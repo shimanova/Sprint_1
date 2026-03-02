c0b9bf8a9ddfd612cc67c51400a5081365c3c8d1
shimanova <m5206596@gmail.com>
2026-03-02 20:43:45 +0300

    task_3.py

--- task_3.py
+++ task_3.py
@@ -13,4 +13,3 @@ if country in world_champions.values():
    print(f'{country} становилась чемпионом мира по футболу в 21 веке!')
 else:
    print(f'{country} не выигрывала чемпионат мира по футболу в 21 веке.')
-    
