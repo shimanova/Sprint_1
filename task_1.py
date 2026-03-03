925e1a3bea2166184d0abc299cae5e391a1a4464
unknown <m5206596@gmail.com>
2026-03-03 22:09:35 +0300

    first commit

---
+++ task_1.py
@@ -0,0 +1,18 @@
+time = '1h 45m,360s,25m,30m 120s,2h 60s'
+time_values = time.split(',')
+total_minutes = 0
+for time_value in time_values:
+    parts = time_value.split()
+    current_minutes = 0
+    for part in parts:
+        if 'h' in part:
+            hours = int(part.replace('h', ''))
+            current_minutes += hours * 60
+        elif 'm' in part:
+            minutes = int(part.replace('m', ''))
+            current_minutes += minutes
+        elif 's' in part:
+            seconds = int(part.replace('s', ''))
+            current_minutes += seconds // 60
+    total_minutes += current_minutes
+print(f'Общее количество минут: {total_minutes}') 
---
+++ task_3.py
@@ -0,0 +1,15 @@
+world_champions = {
+    2002: 'Бразилия',
+    2006: 'Италия',
+    2010: 'Испания',
+    2014: 'Германия',
+    2018: 'Франция',
+    }
+world_champions[2022] = 'Аргентина'
+for year, country_name in world_champions.items():
+   print(f'{year} - {country_name}')
+country = 'Италия'
+if country in world_champions.values():
+   print(f'{country} становилась чемпионом мира по футболу в 21 веке!')
+else:
+   print(f'{country} не выигрывала чемпионат мира по футболу в 21 веке.') 
